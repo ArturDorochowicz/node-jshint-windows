911a231d1880e96437da281fc94d5c8aa3546e07
Artur Dorochowicz <artur@dorochowicz.com>
2011-08-02 13:43:37 +0200

    Include node-jshint LICENSE file. Clean the 'build' script a bit.

--- build.ps1
+++ build.ps1
@@ -5,31 +5,28 @@ $buildDir = "$slnDir/build"
 $node = "$slnDir/node.exe"
 
 if (Test-Path $buildDir) {
-	Remove-Item -Recurse -Force "$buildDir/*"
+	Remove-Item -Recurse -Force -Path "$buildDir/*"
 } else {
-	New-Item -ItemType 'container' $buildDir 
+	New-Item -ItemType container -Path $buildDir 
 }
 
 git submodule update --init --recursive
 
-New-Item -ItemType container "$buildDir/node_modules"
+New-Item -ItemType container -Path "$buildDir/node_modules"
 
-New-Item -ItemType container "$buildDir/node_modules/argsparser"
-New-Item -ItemType container "$buildDir/node_modules/argsparser/lib"
+New-Item -ItemType container -Path "$buildDir/node_modules/argsparser"
+Copy-Item -Include @('index.js', 'package.json') -Path "$slnDir/node-argsparser/*" -Destination "$buildDir/node_modules/argsparser/"
+New-Item -ItemType container -Path "$buildDir/node_modules/argsparser/lib"
 Copy-Item -Path "$slnDir/node-argsparser/lib/argsparser.js" -Destination "$buildDir/node_modules/argsparser/lib/"
-Copy-Item -Path "$slnDir/node-argsparser/index.js" -Destination "$buildDir/node_modules/argsparser/"
-Copy-Item -Path "$slnDir/node-argsparser/package.json" -Destination "$buildDir/node_modules/argsparser/"
 
-New-Item -ItemType 'container' "$buildDir/node_modules/jshint"
-Copy-Item -Path "$slnDir/node-jshint/package.json" -Destination "$buildDir/node_modules/jshint/"
-Copy-Item -Path "$slnDir/node-jshint/HELP" -Destination "$buildDir/node_modules/jshint/"
-Copy-Item -Recurse -Path "$slnDir/node-jshint/lib" -Destination "$buildDir/node_modules/jshint/"
-New-Item -ItemType container "$buildDir/node_modules/jshint/packages/jshint"
+New-Item -ItemType container -Path "$buildDir/node_modules/jshint"
+Copy-Item -Recurse -Include @('lib', 'HELP', 'LICENSE', 'package.json') -Path "$slnDir/node-jshint/*" -Destination "$buildDir/node_modules/jshint/"
+New-Item -ItemType container -Path "$buildDir/node_modules/jshint/packages/jshint"
 Copy-Item -Path "$slnDir/node-jshint/packages/jshint/jshint.js" -Destination "$buildDir/node_modules/jshint/packages/jshint/"
 
 Copy-Item -Path "$slnDir/src/*" -Destination $buildDir
 
-if (!(Test-Path $node)) {
+if (!(Test-Path -Path $node)) {
 	Import-Module BitsTransfer
 	try {
 		Start-BitsTransfer -DisplayName "Downloading node from $nodeUrl" -Source $nodeUrl -Destination $node
