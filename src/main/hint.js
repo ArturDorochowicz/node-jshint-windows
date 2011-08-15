3a97695a7fad20e612a4f3c43a79b34e8e7f493d
Artur Dorochowicz <artur@dorochowicz.com>
2011-08-15 12:52:09 +0200

    Fix fail to run jshint from a different directory than the one containing jshin, node, etc.

--- src/main/hint.js
+++ src/main/hint.js
@@ -1,1 +1,1 @@
-require('./node_modules/jshint/lib/cli').interpret(process.argv);
+require('./node_modules/jshint/lib/cli').interpret(['node'].concat(process.argv.slice(1)));
--- src/main/jshint.bat
+++ src/main/jshint.bat
@@ -1,1 +1,1 @@
-@node "hint.js" %*
+@"%~dp0node.exe" "%~dp0hint.js" %*
