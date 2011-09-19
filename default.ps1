bbda26b4786c789f972985a59b24ddce79b76c3a
Artur Dorochowicz <artur@dorochowicz.com>
2011-09-19 22:24:58 +0200

    Preparation tasks now have to be run manually (initialize-dev task). They are removed from build tasks.

--- default.ps1
+++ default.ps1
@@ -1,27 +1,38 @@
 properties {
 	${node.url} = 'http://nodejs.org/dist/v0.5.4/node.exe'
 
-	${sln.dir} = '.'	
+	${sln.dir} = '.'
 	${build.dir} = "${sln.dir}/build"
 	${src.dir} = "${sln.dir}/src"
 	${lib.build.dir} = "${sln.dir}/lib/build"
 	${lib.runtime.dir} = "${sln.dir}/lib/runtime"
-	${node} = "${lib.runtime.dir}/node/node.exe"	
+	${node} = "${lib.runtime.dir}/node/node.exe"
 }
 
-task default -depends nuget-package
+task default -depends clean, build-cli, nuget-package
 
-task clean -depends clean-build-dir, clean-node
+task initialize-dev -depends update-git-submodules, download-node 
+
+task update-git-submodules {
+	"Running git:`ngit submodule update --init --recursive"
+	exec { git submodule update --init --recursive }
+}
+
+task download-node {
+	${node.dir} = Split-Path -Parent -Path $node
+	if (!(Test-Path ${node.dir})) {
+		New-Item -ItemType container -Path ${node.dir}
+	}
+	Remove-Module BitsTransfer -ErrorAction SilentlyContinue
+	Import-Module BitsTransfer	
+	Start-BitsTransfer -DisplayName "Downloading node from ${node.url}" -Source ${node.url} -Destination $node
+}
+
+task clean -depends clean-build-dir
 
 task clean-build-dir {
 	if (Test-Path -Path ${build.dir}) {
 		Remove-Item -Recurse -Force -Path ${build.dir}
-	}
-}
-
-task clean-node {
-	if (Test-Path -Path ${node}) {
-		Remove-Item -Force -Path ${node}
 	}
 }
 
@@ -31,12 +42,7 @@ task prepare-build-dir {
 	}
 }
 
-task git-submodules-update {
-	"Running git:`ngit submodule update --init --recursive"
-	exec { git submodule update --init --recursive }
-}
-
-task build-cli -depends clean-build-dir, prepare-build-dir, git-submodules-update, download-node {
+task build-cli -depends prepare-build-dir {
 	New-Item -ItemType container -Path "${build.dir}/cli"
 
 	Copy-Item -Recurse -Path "${src.dir}/main/*" -Destination "${build.dir}/cli"
@@ -54,20 +60,6 @@ task build-cli -depends clean-build-dir, prepare-build-dir, git-submodules-updat
 	Copy-Item -Path "${lib.runtime.dir}/node-jshint/packages/jshint/jshint.js" -Destination "${build.dir}/cli/lib/node_modules/jshint/packages/jshint/"
 }
 
-task download-node {
-	if (Test-Path -Path $node) {
-		"Skipping node download. Node already exists at: ${node}"
-	} else {
-		Remove-Module BitsTransfer -ErrorAction SilentlyContinue
-		Import-Module BitsTransfer
-		${node.dir} = Split-Path -Parent -Path $node
-		if (!(Test-Path ${node.dir})) {
-			New-Item -ItemType container -Path ${node.dir}
-		}
-		Start-BitsTransfer -DisplayName "Downloading node from ${node.url}" -Source ${node.url} -Destination $node
-	}
-}
-
 task nuget-package-layout -depends build-cli {
 	New-Item -ItemType container -Path "${build.dir}/nuget-package-layout"
 	New-Item -ItemType container -Path "${build.dir}/nuget-package-layout/tools"
@@ -82,4 +74,4 @@ task nuget-package -depends nuget-package-layout {
 }
 
 include "./src/test/tests.ps1"
-task test -depends build-cli, run-tests
+task test -depends clean, build-cli, run-tests
