05f7d19357c20f354625a011aa6dacf9ebe389c2
Artur Dorochowicz <artur@dorochowicz.com>
2011-10-24 21:35:18 +0200

    Update test reporter.

--- src/test/error-count-reporter.js
+++ src/test/error-count-reporter.js
@@ -1,9 +1,5 @@
 module.exports = {
     reporter: function (results) {
-        process.stdout.on('end', function () {
-            process.exit(results.length > 0 ? 1 : 0);
-        });
-
         process.stdout.write("Errors: " + results.length + "\n");
     }
 };
